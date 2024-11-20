b0fb0620e544b0d5a91a9eec6bec50d591974d5f
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2024-11-20 13:18:47 -0500

    Backgorund + Colors + Gameover
    Adds Background as well as create different levels and colors. Fixed block ordering. Needs to fix game over script.

--- Scripts/lvl.gd
+++ Scripts/lvl.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-@onready var brickObj = preload("res://Scenes/block.tscn")
+@onready var blockObj = preload("res://Scenes/block.tscn") #Loads block into scene
 
-var columns = 20
+var columns = 22
 var rows = 7
-var margin = 50 #Margin is allowing a little gap in between the bricks
+var margin = 50 #Margin is allowing a little gap in between the blocks
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,20 +16,32 @@ func _process(delta: float) -> void:
 
 func setupLevel():
 	
+	var colors = _getColors() #calls colors func so that we can use it to change blocks
+	colors.shuffle() #shuffles color order
+	
 	for r in rows:
 		for c in columns: #loops through each row and column
-			
-			var newBrick = brickObj.instantiate() #creates obj in game
-			add_child(newBrick) #adds as child to lvl
-			newBrick.position = Vector2(margin + (34 * c), margin + (34 * r))
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+			var randomNumber = randi_range(0, 2) # gives us random #between 0,1,&2
+			if randomNumber > 0: #now this randomizes block placement
+				
+				var newBlock = blockObj.instantiate() #creates obj in game
+				add_child(newBlock) #adds as child to lvl
+				newBlock.position = Vector2(margin + (50 * c), margin + (34 * r))
+				
+				var sprite = newBlock.get_node('Sprite2D') #reference to sprite
+				if r <= 9: #sets up color changing by rows
+					sprite.modulate = colors[0]
+				if r < 6:
+					sprite.modulate = colors[1]
+				if r < 3:
+					sprite.modulate = colors[2]
+
+
+func _getColors():
+	var colors = [ #Array of colors to choose from for blocks
+		Color(0, 1, 1, 1,),
+		Color(0.54, 0.17, 0.89, 1),
+		Color(0.68, 1, 0.18, 1),
+		Color(1, 1, 1, 1),
+	]
+	return colors
