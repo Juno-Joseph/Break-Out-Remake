8d12c9a1d278e962e61f23711ca04fa712850a5f
Juno Joseph <87766026+Juno-Joseph@users.noreply.github.com>
2024-11-19 20:50:37 -0500

    ball paddle and block
    Finishing up ball paddle and block scripting

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -14,8 +14,12 @@ func _physics_process(delta):
 		if collision: #when ball collides with paddle
 			velocity = velocity.bounce(collision.get_normal())#gives us bounce direction for ball and sets vel for opposite direction
 			
+			if collision.get_collider().has_method("hit"):
+				collision.get_collider().hit() #calls hit function on brick when abll hit it
+			
 		if(velocity.y > 0 and velocity.y < 100):# prevents ball from just boucing left and right continuously beacuse the vertival velocity would be to slow to make a difference
 			velocity.y = -200 #if velocity y is to low we shoot it up a little bit
 		
 		if(velocity.x == 0): #prevents ball from just boucing up and down continuously beacuse the horizontal velocity would be to slow to make a difference
 			velocity.x = -200 #if velocity x is to low we will shoot it right a bit
+		
--- Scripts/block.gd
+++ Scripts/block.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,7 +10,10 @@ func _ready():
 func _process(_delta):
 	pass
 	
-func _on_body_entered(body):
-	if body.is_in_group("ball"):
-		print("hit block")
-		queue_free()
+func hit():
+	
+	$Sprite2D.visible = false
+	$CollisionShape2D.disabled = true
+	
+	await get_tree().create_timer(1).timeout
+	queue_free()
